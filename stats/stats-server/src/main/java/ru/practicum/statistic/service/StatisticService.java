687ea8cf2da4ec39bbc3c17655c2f2f0c9d1d528
Макс <maksim5494@yandex.ru>
2026-05-19 03:31:21 +0500

    1

--- stats/stats-server/src/main/java/ru/practicum/statistic/service/StatisticService.java
+++ stats/stats-server/src/main/java/ru/practicum/statistic/service/StatisticService.java
@@ -4,7 +4,6 @@ package ru.practicum.statistic.service;
 import ru.practicum.dto.StatisticDto;
 import ru.practicum.dto.StatisticResponse;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatisticService {
